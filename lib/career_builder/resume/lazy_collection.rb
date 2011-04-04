8ea234021beb9287043fa9a5befb35b40b986678
Michael Guterl <mguterl@gmail.com>
2011-04-04 09:22:49 -0400

    retry 5 times on connection reset errors

--- lib/career_builder/resume/lazy_collection.rb
+++ lib/career_builder/resume/lazy_collection.rb
@@ -7,26 +7,34 @@ module CareerBuilder
     def initialize(client, options)
       @client = client
       @search_options = options
+      @current_page = @search_options[:page] || 1
     end
 
     def each
-      current_page = search_options[:page] || 1
-
-      search = client.advanced_resume_search(search_options.merge(:page_number => current_page, :rows_per_page => 500))
-
-      results = search.results
+      results = results_for @current_page
 
       loop do
         results.each do |resume|
           yield Resume.new(client, resume)
         end
 
-        current_page += 1
+        @current_page += 1
 
-        search = client.advanced_resume_search(search_options.merge(:page_number => current_page, :rows_per_page => 500))
-        results = search.results
+        results = results_for @current_page
         break if results.empty?
       end
+    end
+
+    private
+
+    def results_for(page)
+      attempts = 0
+      search = client.advanced_resume_search(search_options.merge(:page_number => page, :rows_per_page => 500))
+      search.results
+    rescue Errno::ECONNRESET => e
+      attempts += 1
+      retry if attempts < 5
+      raise e
     end
 
   end
